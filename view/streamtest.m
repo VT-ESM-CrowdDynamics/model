355fca2d3cd73a2b2c65f78d4fde38834bb05585
Hunter Morgan <automaticgiant@gmail.com>
2015-02-24 16:53:20 -0500

    prototype liveview and test data

--- view/streamtest.m
+++ view/streamtest.m
@@ -1,9 +1,53 @@
+% profile on
+
+%normally 60/120 hz
+frameskip = 200;
 for a = 1:11
 	input('', 's');
 end
+hold on
+axis([-10000 10000 -10000 10000])
 line = input('', 's');
-while !(strcmp(line, ''))
+frame = 0;
+while !(strcmp(line, '') || frame > 3000)
+	% disp('main loop')
 	fields = strsplit(line,'\t','CollapseDelimiters',false);
-	disp(fields);
+	% disp(fields(1:2))
+	if mod(frame++, frameskip + 1) == 0
+		% next line only good if frame/time was exported from QTM
+		fprintf('Frame: %1s, Time: %2s\n', fields{1}, fields{2})
+
+		% start columns need to reflect time/frame output
+		% skip value needs to reflect presence of z measurements
+		Xcoords = str2double(fields(3:3:end));
+		Ycoords = str2double(fields(4:3:end));
+		% Xcoords = str2double(fields(1:3:end));
+		% Ycoords = str2double(fields(2:3:end));
+
+		% this seems (empirically) necessary, but not sure about reasoning
+		Xcoords = Xcoords(~isnan(Xcoords));
+		Ycoords = Ycoords(~isnan(Ycoords));
+		% check lengths
+		% disp(length(Xcoords))
+		% disp(length(Ycoords))
+
+		% Xcoords = Xcoords(cellfun('isnumeric',fields(3:2:end)))
+		% Ycoords = Ycoords(cellfun('isnumeric',fields(4:2:end)))
+
+		% remove last plotted frame or disable to see tracks
+		if exist('last')
+			delete(last);
+		end
+		last = plot(Xcoords, Ycoords, 'linestyle', 'none', 'marker', '.');
+		drawnow;
+	end
 	line = input('', 's');
 end
+
+% results=profile('info');
+% show 50 most time spent calls
+% profshow(results, 50)
+
+% dont work because results is a struct with two structs? or struct arrays? idk
+% csvwrite('FunctionTable', results.FunctionTable)
+% csvwrite('Heirarchical', results.Heirarchical)
