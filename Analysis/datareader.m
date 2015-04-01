cdc3fc104ce2594d36ed94d22d3d9f7e1bd5bd76
wjosam87 <wjosam87@vt.edu>
2015-03-31 20:49:55 -0400

    Combined for loops
    The for loops that compute the IPdistance and crowddensity have been
    comined.

--- Analysis/datareader.m
+++ Analysis/datareader.m
@@ -35,10 +35,14 @@ for p=1:nummarker
 end
 
 % Crowd Density
-crowddensity=zeros(timespan,5); %6th column is non-counted space (rooms, etc)
+crowddensity=zeros(5,timespan); 
 % peoplecount=zeros(timespan,5);
 
-for t=1:timespan
+IPdistance=zeros(timespan, nummarker); % This array stores the interpersonal distance for each marker at each point in time.
+IPstd=zeros(timespan, nummarker); % This array stores all the Std values for the interpersonal distance.
+Closest=zeros(timespan,nummarker);
+
+parfor t=1:timespan
     %This will count the number of markers in each group area at each point
     %in time.
     
@@ -56,7 +60,7 @@ for t=1:timespan
             
         grp1=grp1+1;
         
-        elseif marker(h,t,1)> -1280 && marker(h,t,1)< 1291 &...
+        elseif marker(h,t,1)> -1280 && marker(h,t,1)< 1291 &&...
                 marker(h,t,2)< 978 && marker(h,t,2)> -1460
             
         grp2=grp2+1;
@@ -77,55 +81,45 @@ for t=1:timespan
         grp5=grp5+1;
         
         end  % end of the if / else if statement
-    end %end of iterating through all markers
-    
-    crowddensity(t,1)=grp1/6.9216; %All the densities are measured in people/m^2
-    crowddensity(t,2)=grp2/6.2686;
-    crowddensity(t,3)=grp3/7.2138;
-    crowddensity(t,4)=grp4/7.212;
-    crowddensity(t,5)=grp5/7.2887;
     
 %     peoplecount(t,1)=grp1;  % These are the totals counted in each group.
 %     peoplecount(t,2)=grp2;
 %     peoplecount(t,3)=grp3;
 %     peoplecount(t,4)=grp4;
 %     peoplecount(t,5)=grp5;
-%     
-end
 
 % Interpersonal Distance
 % The goal of this function is to determine the five closest markers and
 % then determine the average distance from our goal marker to its 5 nearest
 % friends. 
 
-IPdistance=zeros(timespan, nummarker); % This array stores the interpersonal distance for each marker at each point in time.
-IPstd=zeros(timespan, nummarker); % This array stores all the Std values for the interpersonal distance.
-Closest=zeros(timespan,nummarker);
-for a=1:timespan
-    for b=1:nummarker
         alldist=NaN(nummarker,1);
-        if marker(b,a,1)==0.0 && marker(b,a,2)==0.0;
+        if marker(h,t,1)==0.0 && marker(h,t,2)==0.0;
             
         else
-        posx=marker(b,a,1); % X-position of marker in q.
-        posy=marker(b,a,2); % Y-Position of marker in q.
+        posx=marker(h,t,1); % X-position of marker in q.
+        posy=marker(h,t,2); % Y-Position of marker in q.
         for c=1:nummarker
             
-            if marker(c,a,1)==0.0 && marker(c,a,2)==0.0;
+            if marker(c,t,1)==0.0 && marker(c,t,2)==0.0;
                 
-            elseif c~=b
-                alldist(c,1)=sqrt((posx-marker(c,a,1))^2+(posy-marker(c,a,2))^2);
+            elseif c~=h
+                alldist(c,1)=sqrt((posx-marker(c,t,1))^2+(posy-marker(c,t,2))^2);
             else
             end
         end
         alldist=sort(alldist);
         end
         avedist=(alldist(1)+alldist(2)+alldist(3)+alldist(4)+alldist(5))/5; % This is the average distance between our marker and the closest 5 markers.
-        avestd=std(alldist(1:5)); % This is the stand. Dev. between the above markers.
-        IPdistance(a, b)=avedist;
-        IPstd(a,b)=avestd;
-        Closest(a,b)=alldist(1);
-    end
+        avestd=std(alldist(1:5)); % This is the st. Dev. between the above markers.
+        IPdistance(t, h)=avedist;
+        IPstd(t,h)=avestd;
+        Closest(t,h)=alldist(1);
+        
+    end %end of iterating through all markers
+ 
+    crowddensity(:,t)=[grp1/6.9216, grp2/6.286, grp3/7.2138, grp4/7.212, grp5/7.2287]; %All the densities are measured in people/m^2
+    
 end
 
 % Flux
